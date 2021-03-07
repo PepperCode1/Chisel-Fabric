111a1b3c71cbb3e0cc66d8f5fef5e7653c382fd3
PepperBell <44146161+PepperCode1@users.noreply.github.com>
2021-03-06 16:07:21 -0800

    Make it compile

--- src/main/java/team/chisel/chisel/client/init/Screens.java
+++ src/main/java/team/chisel/chisel/client/init/Screens.java
@@ -1,13 +1,15 @@
 package team.chisel.chisel.client.init;
 
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import team.chisel.chisel.client.gui.ChiselScreen;
 import team.chisel.chisel.client.gui.HitechChiselScreen;
 import team.chisel.chisel.init.ScreenHandlers;
+import team.chisel.chisel.inventory.ChiselScreenHandler;
 
 public class Screens {
 	public static void init() {
-		ScreenRegistry.register(ScreenHandlers.NORMAL_CHISEL, ChiselScreen::new);
+		ScreenRegistry.register(ScreenHandlers.NORMAL_CHISEL, (ScreenRegistry.Factory<ChiselScreenHandler, HandledScreen<ChiselScreenHandler>>) ChiselScreen::new);
 		ScreenRegistry.register(ScreenHandlers.HITECH_CHISEL, HitechChiselScreen::new);
 	}
 }
